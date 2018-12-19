3a1582ebe21e719faa18051dd4a847321c8be523
Vinicius Donadon <vinicius.donadon@sunsetcom.com.br>
2018-12-19 15:35:21 -0200

    Introdução a condicionais e listas

---
+++ desafio-04/app.js
@@ -0,0 +1,27 @@
+new Vue({
+  el: '#desafio',
+  data: {
+    primeiraClasse: 'destaque',
+    alterarClasse: 'destaque',
+    classe4: '',
+    negative: true,
+    cor: '',
+    widthPerSec: 0
+  },
+  methods: {
+    iniciarEfeito(){
+      setInterval(() => {
+        this.primeiraClasse = this.primeiraClasse == 'destaque' 
+        ? 'encolher' : 'destaque'
+      }, 1000)
+    },
+    iniciarProgresso(){
+      let valor = 0
+      const temporizador = setInterval(() => {
+        valor += 5
+        this.widthPerSec = `${valor}%`
+        if (valor == 100){ clearInterval(temporizador) }
+      }, 0500)
+    }
+  }    
+});
