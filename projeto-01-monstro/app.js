624a8b0f4e2510e156f6853c893231b3794a8d69
Vinicius Donadon <vinicius.donadon@sunsetcom.com.br>
2018-12-20 16:58:21 -0200

    Iniciado projeto

---
+++ projeto-01-monstro/app.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app',
+    data: {
+        running: false,
+        playerLife: 0,
+        monsterLife: 10,
+
+    },
+    computed: {
+        hasResult(){
+            return this.playerLife <= 0 || this.monsterLife <= 0
+        },
+    },
+    methods: {
+
+    },
+    watch: {
+
+    }
+}) 
