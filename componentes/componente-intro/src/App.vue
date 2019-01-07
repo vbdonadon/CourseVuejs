e0834ba5351bcc5e6c8a4be19c0200ba89959b59
Vinicius Donadon <vinicius.donadon@sunsetcom.com.br>
2019-01-07 09:55:42 -0200

    Modulo de Componentes (Iniciante)

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" style="font-family: Gotham">
+    <h2>Contadores:</h2>
+    <p>{{contador}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        contador: 0
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ componentes/componente-intro/src/Contador.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="contador">
+        <span>{{contador}}</span>
+        <button @click="adicionar">+</button>
+        <button @click="subtrair">-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            contador: 0
+        }
+    },
+    
+}
+</script>
+
+<style>
+
+</style>
